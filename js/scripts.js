64d5063d3646f1005d7791e0db6f08cc68fba156
AngelHernadezC137 <angelinfinite9@gmail.com>
2024-06-10 12:36:26 -0700

    Organized code and added a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 let pokemonList= [
-    {name: 'Dragonite', height: 7, type:['Dragon','Flying']},
-    {name: 'Salazzle', height: 5, type:['Poison','Fire']},
-    {name: 'Giritina', height: 8, type:['Dragon','Ghost']},
+    {  name: 'Dragonite',
+       height: 7, 
+       type:['Dragon','Flying']},
+
+    {  name: 'Salazzle', 
+       height: 5, 
+       type:['Poison','Fire']},
+
+    {  name: 'Giritina', 
+       height: 8, 
+       type:['Dragon','Ghost']},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+    if (pokemon.height > 6) {
+        document.write(" - Wow, that\'s Big!");
+    }
+    document.write("</br>");
+};
